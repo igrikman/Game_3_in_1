eac47c350b9e94c00266811d54b1bf8db248d0fc
DESKTOP-PSPE6EH\lider <lider.x2011@yandex.ru>
2021-10-10 18:40:15 +0300

    1_Games v0.02

---
+++ Game_3_in_1/Assets/Scripts/Sc_Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sc_Controller : MonoBehaviour
+{
+    private float spawnTime = 3f;
+    [SerializeField] private List<Sc_Enemy> enemyList;
+    [SerializeField] private Transform[] spawnPoint;
+    [SerializeField] private Transform player;
+    [SerializeField] private Sc_Enemy enemyPrefab;
+
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        enemyList = new List<Sc_Enemy>();
+    }
+    void Update()
+    {
+        foreach (var enemy in enemyList)
+        {
+            if (enemy.isDestroy)
+            {
+                RemoveEnemy(enemy);
+            }
+        }
+    }
+    public void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoint.Length);
+        var enemy = Instantiate(enemyPrefab, spawnPoint[spawnPointIndex].position, spawnPoint[spawnPointIndex].rotation);
+        enemyList.Add(enemy);
+    }
+
+    public void RemoveEnemy(Sc_Enemy enemy)
+    {
+        Debug.Log("Destroy enemy");
+        enemyList.Remove(enemy);
+
+        Destroy(enemy.gameObject);
+    }
+
+
+}
---
+++ Game_3_in_1/Assets/Scripts/Sc_Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sc_Enemy : MonoBehaviour
+{
+    public bool isDestroy = false;
+    [SerializeField] private Transform player;
+    [SerializeField] private float speed;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();   
+    }
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime); 
+    }
+    public void OnMouseDown()
+    {
+        Debug.LogError("OnMouseDown");
+        isDestroy = true;
+    }
+}
