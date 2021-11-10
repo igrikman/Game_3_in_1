1d6b38f7f3c548cbe72f8609997b1bc1f097af74
DESKTOP-PSPE6EH\lider <lider.x2011@yandex.ru>
2021-11-10 13:17:16 +0300

    Base_and_OOP

---
+++ Game_3_in_1/Assets/Scripts/Base_and_OOP.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base_and_OOP : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+
+
+    #region Base
+
+
+
+
+    #endregion Base
+}
